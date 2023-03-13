d111d458a93ff8c947dbd2bf356ea47e1ac4a033
Arun <aruneesh4@gmail.com>
2023-03-13 23:33:30 +0530

    KYC steps page added

---
+++ src/steps.js
@@ -0,0 +1,37 @@
+import './App.css';
+import {CheckCircleOutlined} from '@ant-design/icons';
+import { Steps } from 'antd';
+const description = 'This is a description.';
+function App() {
+  return (
+    <div className="App">
+        <Steps
+          className="custome-step"
+          id="check"
+          direction="vertical"
+            size="small"
+            current={1}
+            items={[
+              {
+                title: 'Signed into Vittae app',
+                description,
+                icon: <CheckCircleOutlined />,
+                
+              },
+              {
+                title: 'Started KYC',
+                description,
+                icon: <CheckCircleOutlined />
+              },
+              {
+                title: 'Plan upload',
+                description,
+                icon: <CheckCircleOutlined />
+              },
+            ]}
+        />
+    </div>
+  );
+}
+
+export default App;
