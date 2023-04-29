c8f4ee86483b30c095f62baae10aa03e3cca5e02
Dipp3r <aruneesh4@gmail.com>
2023-04-28 19:45:33 -0700

    profile image replaced with letters

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -62,7 +62,7 @@ class DashboardComp extends React.Component{
         <section id="dashboard">
             <nav className="navbar" >
               <button className="profile" onClick={this.props.navigate} value="../profile">
-                <p id="profileImg">{`${localStorage.getItem("first_name")[0].toUpperCase()}`}</p>
+                <p id="profileName">{`${localStorage.getItem("first_name")[0].toUpperCase()}K`}</p>
               </button>
           
               <div className="icons">
